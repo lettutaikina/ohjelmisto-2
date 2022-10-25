98519de0f458c87c6c79cb6d07a4db9332a2029b
leevi <leevi.laasonen@metropolia.fi>
2022-10-25 15:04:05 +0300

    9.1

---
+++ moduuli 9/9.1.py
@@ -0,0 +1,10 @@
+class Auto:
+    def __init__(self, rek, Hnop, ATMnop, Kmatka):
+        self.rek = rek
+        self.Hnop = Hnop
+        self.ATMnop = 0
+        self.Kmatka = 0
+        print(f"auton rekisterinumero on {self.rek}\nhuippunopeus on {self.Hnop}\nnopeus on {self.ATMnop}\nkokonaismatka {self.Kmatka}")
+        return
+#pääohjelma
+auto1=Auto("ABC-123",143, "", "" )
