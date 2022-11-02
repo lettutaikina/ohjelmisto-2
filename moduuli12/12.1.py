98c0ff61f43f5aea65c7620b9d2be50a37cfc661
leevi <leevi.laasonen@metropolia.fi>
2022-11-02 15:15:35 +0200

    9.3

---
+++ moduuli12/12.1.py
@@ -0,0 +1,8 @@
+import requests
+
+hakusana = input("Anna hakusana: ")
+
+# Pyynnön malli: https://api.tvmaze.com/search/shows?q=girls
+pyyntö = "https://api.tvmaze.com/search/shows?q=" + hakusana
+vastaus = requests.get(pyyntö).json()
+print(vastaus)
